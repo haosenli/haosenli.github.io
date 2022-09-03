f03c8eab617f10ae33cf9e4f789579c2cd149ab0
Haosen <haosenli2001@gmail.com>
2022-09-02 17:31:51 -0700

    Changed dark mode time.

--- js/theme.js
+++ js/theme.js
@@ -30,7 +30,8 @@ async function autoThemeMode() {
     while (autoTheme.classList.contains('active')) {
         let d = new Date();
         let hr = d.getHours();
-        if (hr >= 17) {
+        // dark mode at or after 6PM
+        if (hr >= 18) {
             darkThemeMode();
         } else {
             lightThemeMode();
